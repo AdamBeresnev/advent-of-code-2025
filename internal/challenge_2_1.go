8f2d1ab3b9c06b74ea7c0d1e7430106b777675e4
AdamBeresnev <adam.beresnev@gmail.com>
2025-12-04 01:55:04 +0200

    redid day2challenge1 function according to challenge rules

--- internal/challenge_2_1.go
+++ internal/challenge_2_1.go
@@ -1,37 +1,41 @@
 package internal
 
 import (
-	// "bufio"
 	"bytes"
+	"encoding/csv"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 )
 
 func Day2Challenge1 (file *os.File) {
 	var result int
-	// scanner := bufio.NewReader(file)
-	
-	// File read logic
+	reader := csv.NewReader(file)
+	idRangeList, err := reader.ReadAll()	
+	if err != nil {
+		log.Fatal("Could not read input\n", err)
+	}
+	idRanges := idRangeList[0]
+
+	for _, idRange := range idRanges {
+		result += invalidIdSum(idRange)
+	}
 
 	fmt.Println(result)
 }
 
-func invalidIdsInRange (idRange string) int {
-	var invalidIds int
-
-	if idRange[len(idRange) - 1:] == "," {
-		idRange = idRange[:len(idRange) - 1]
-	}
-
-	rangeSeparatorPos := bytes.IndexByte([]byte(idRange), ':')
+func invalidIdSum (idRange string) int {
+	var sum int
+	
+	rangeSeparatorPos := bytes.IndexByte([]byte(idRange), '-')
 	
 	rangeStart, err := strconv.Atoi(idRange[:rangeSeparatorPos])
 	if err != nil {
 		log.Fatal("Couldn't read starting position in range", idRange)
 	}
-
+	
 	rangeEnd, err := strconv.Atoi(idRange[rangeSeparatorPos + 1:])
 	if err != nil {
 		log.Fatal("Couldn't read ending position in range", idRange)
@@ -40,33 +44,74 @@ func invalidIdsInRange (idRange string) int {
 	lenRangeStart := intLength(rangeStart)
 	lenRangeEnd := intLength(rangeEnd)
 
-	halfLengthStart := lenRangeStart / 2
-	halfLengthEnd := lenRangeEnd / 2
-
+	var multiplier int
+	var invalidId int
 	for i := lenRangeStart; i <= lenRangeEnd; i++ {
-		if i % 2 == 0 {
-			invalidIds += 9 * 10 ^ (i / 2 - 1)
+		if i % 2 != 0 {
+			continue
+		}
+		
+		multiplier = int(math.Pow10(i/2))
+
+		for o := multiplier/10; o < multiplier; o++ {
+			invalidId = o * multiplier + o
+			
+			if invalidId > rangeEnd {
+				break
+			}
+			
+			if invalidId >= rangeStart {
+				sum += invalidId
+			}
 		}
 	}
-	
-	if lenRangeStart % 2 == 0 {
-		invalidIds -= (rangeStart - 10 ^ (lenRangeStart - 1)) / 10 ^ halfLengthStart 
-
-		if rangeStart / 10 ^ halfLengthStart < rangeStart % 10 ^ halfLengthStart {
-			invalidIds--
-		}
-	}
-	
-	if lenRangeEnd % 2 == 0 {
-		invalidIds -= (10 ^ lenRangeEnd - rangeEnd) / 10 ^ halfLengthEnd 
-
-		if rangeEnd / 10 ^ halfLengthEnd > rangeEnd % 10 ^ halfLengthEnd {
-			invalidIds--
-		}
-	}
-
-	return invalidIds
+	return sum
 }
+
+// func invalidIdsInRange (idRange string) int {
+// 	var invalidIds int
+
+// 	rangeSeparatorPos := bytes.IndexByte([]byte(idRange), '-')
+	
+// 	rangeStart, err := strconv.Atoi(idRange[:rangeSeparatorPos])
+// 	if err != nil {
+// 		log.Fatal("Couldn't read starting position in range", idRange)
+// 	}
+	
+// 	rangeEnd, err := strconv.Atoi(idRange[rangeSeparatorPos + 1:])
+// 	if err != nil {
+// 		log.Fatal("Couldn't read ending position in range", idRange)
+// 	}
+	
+// 	lenRangeStart := intLength(rangeStart)
+// 	lenRangeEnd := intLength(rangeEnd)
+	
+// 	halfLengthStart := lenRangeStart / 2
+// 	halfLengthEnd := lenRangeEnd / 2
+	
+// 	for i := lenRangeStart; i <= lenRangeEnd; i++ {
+// 		if i % 2 == 0 {
+// 			invalidIds += 9 * int(math.Pow10(i / 2 - 1))
+// 		}
+// 	}
+	
+// 	if lenRangeStart % 2 == 0 {
+// 		invalidIds -= (rangeStart - int(math.Pow10(lenRangeStart - 1))) / int(math.Pow10(halfLengthStart)) 
+
+// 		if rangeStart / int(math.Pow10(halfLengthStart)) < rangeStart % int(math.Pow10(halfLengthStart)) {
+// 			invalidIds--
+// 		}
+// 	}
+	
+// 	if lenRangeEnd % 2 == 0 {
+// 		invalidIds -= (int(math.Pow10(lenRangeEnd)) - rangeEnd) / int(math.Pow10(halfLengthEnd)) 
+
+// 		if rangeEnd / int(math.Pow10(halfLengthEnd)) > rangeEnd % int(math.Pow10(halfLengthEnd)) {
+// 			invalidIds--
+// 		}
+// 	}
+// 	return invalidIds
+// }
 
 func intLength (number int) int {
 	if number == 0 {
