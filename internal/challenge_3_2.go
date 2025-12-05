df302a44588b9fc3db3c1fed3bb91e1d91214735
AdamBeresnev <adam.beresnev@gmail.com>
2025-12-05 02:08:38 +0200

    bugfix

--- internal/challenge_3_2.go
+++ internal/challenge_3_2.go
@@ -11,13 +11,11 @@ import (
 func Day3Challenge2 (file *os.File) {
 	var sum int
 	var bank []int
-	var max int
 
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		// line = "234234234234278"
 		
 		for _, v := range line {
 			num, err := strconv.Atoi(string(v))
@@ -29,9 +27,7 @@ func Day3Challenge2 (file *os.File) {
 			bank = append(bank, num)
 		}
 
-		max = maxJoltageOverride(bank)		
-		fmt.Println(line, max)
-		sum += max
+		sum += maxJoltageOverride(bank)
 		bank = []int{}
 	}
 
@@ -39,13 +35,13 @@ func Day3Challenge2 (file *os.File) {
 }
 
 func maxJoltageOverride (bank []int) int {
-	var joltage int
-	var batteriesLeft int
+	var joltage int = 0
+	var batteriesLeft int = 0
 
 	lenBank := len(bank)
 	maxPositions := []int{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
 
-	for i := 0; i < len(bank) - 12; i++ {
+	for i := 0; i <= len(bank) - 12; i++ {
 		if maxPositions[0] < 0 || bank[maxPositions[0]] < bank[i] {
 			maxPositions[0] = i
 		}
